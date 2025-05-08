c189c6cbd82fc4306aab2e9260f160eab5c2c00d
wassim <wassim.kraiem.ess@gmail.com>
2025-05-08 11:18:47 +0100

    adding third project

--- src/app/components/ProjectsSection.jsx
+++ src/app/components/ProjectsSection.jsx
@@ -10,7 +10,7 @@ const projectsData = [
 		title: 'Next js  Portfolio Website',
 		description:
 			'I built my Portfolio website with Nextjs tailwindcss and Shadcn',
-		stack:'Nextjs tailwindcss Shadcn',
+		stack: 'Nextjs tailwindcss Shadcn',
 		image: '/images/projects/1.png',
 		tag: ['All', 'Web'],
 		gitUrl: '/',
@@ -21,7 +21,7 @@ const projectsData = [
 		title: 'The Wild oasis',
 		description:
 			'Cozy yet luxurious cabins, for you to enjoy with you loved ones',
-		stack:'Nextjs tailwindcss Supabase',
+		stack: 'Nextjs tailwindcss Supabase',
 		image: '/images/projects/2.png',
 		tag: ['All', 'Web'],
 		gitUrl: '/https://github.com/Wassimkraiem/the-wild-next',
@@ -32,8 +32,8 @@ const projectsData = [
 		title: 'Advanced Video Search Api',
 		description:
 			'I built this api in my internship to help with the slow search of some saas',
-		stack:'Flask Dynamodb Opensearch ',
-		image: '/',
+		stack: 'Flask Dynamodb Opensearch ',
+		image: '/images/projects/3.png',
 		tag: ['All', 'Web'],
 		gitUrl: '/',
 		previewUrl: '/',
