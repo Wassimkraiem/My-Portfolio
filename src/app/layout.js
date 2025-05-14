6e24f4e1c6059d59cd2a35b61b6cc13ec613bd96
wassim Kraiem <wassim.kraiem.ess@gmail.com>
2025-05-14 09:35:39 +0100

    latest

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,17 +11,6 @@ export default function RootLayout({ children }) {
 	return (
 		<html lang='en'>
 			<body className={inter.className}>
-				<div className='w-full absolute inset-0 h-screen'>
-					<SparklesCore
-						id='tsparticlesfullpage'
-						background='transparent'
-						minSize={0.6}
-						maxSize={1.4}
-						particleDensity={100}
-						className='w-full h-full'
-						particleColor='#FFFFFF'
-					/>
-				</div>
 				{children}
 			</body>
 		</html>
